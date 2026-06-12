0fc02a5f9acf8fc706668885e4268edc464704e8
ungureanudaniel <ungureanu.daniel86@gmail.com>
2026-06-12 13:31:58 +0300

    added django admin page for registry

--- registries/admin.py
+++ registries/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from pytz import timezone
+from .models import RangerDocumentRegistry
 
-# Register your models here.
+@admin.register(RangerDocumentRegistry)
+class RangerDocumentRegistryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
+    list_filter = ('created_at', 'updated_at')
+    ordering = ('-created_at',)
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',),
+        }),
+    )
+    readonly_fields = ('created_at', 'updated_at')
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_at = timezone.now()
+        obj.updated_at = timezone.now()
+        super().save_model(request, obj, form, change)
+
+
